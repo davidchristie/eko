b60b3490f60ad737d21b1415e28da9def5df8c28
David Christie <davidchristie.nz@gmail.com>
2016-11-26 13:01:49 +1300

    Create eko module

---
+++ js/eko.js
@@ -0,0 +1,18 @@
+/* jshint esversion: 6 */
+
+var eko = (function() {
+  'use strict';
+
+  // Private variables.
+  const internal = (function() {
+    const namespaces = new WeakMap();
+    return function internal(instance) {
+      if (!namespaces.has(instance))
+        namespaces.set(instance, {});
+      return namespaces.get(instance);
+    };
+  })();
+
+  // Public API.
+  return {};
+})();
