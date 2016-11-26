f0862456169d821d64cb8414383415d9aa0e294b
David Christie <davidchristie.nz@gmail.com>
2016-11-26 13:21:25 +1300

    Create model class

--- js/eko.js
+++ js/eko.js
@@ -13,6 +13,32 @@ var eko = (function() {
     };
   })();
 
+  const Model = (function() {
+
+    class Model {
+
+      constructor() {
+      }
+
+      // Return an array of entities matching the specified properties. If no
+      // properties are given, return all entities in the model.
+      entities(properties) {
+        // TODO
+      }
+
+      // If an ID value is given return the corresponding entity. Otherwise
+      // return a new entity that hasn't been added to the model yet.
+      entity(id) {
+        // TODO
+      }
+
+    }
+
+    return Model;
+  });
+
+  console.log(new Model());
+
   // Public API.
   return {};
 })();
